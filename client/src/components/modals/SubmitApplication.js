ab14e3a15165b674d4b2b4408fec97f178d9e640
Anton <antonblkv@yandex.ru>
2024-04-16 18:39:13 +0300

    add html modal on main page

--- client/src/components/modals/SubmitApplication.js
+++ client/src/components/modals/SubmitApplication.js
@@ -1,38 +1,83 @@
-import React, { useState } from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useContext, useEffect, useState } from 'react';
+import { Form } from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal';
-import { Form, Button } from 'react-bootstrap';
-import { createType } from '../../http/serviceAPI';
+import { createService, fetchTypes } from '../../http/serviceAPI';
+import close from '../../images/close.svg';
+import { Context } from '../../index';
+import '../../styles/createService.css';
 
-const SubmitApplication = ({ show, onHide }) => {
-	/*const [value, setValue] = useState('');
+const SubmitApplication = observer(({ show, onHide }) => {
+	const { service } = useContext(Context);
+	const [name, setName] = useState('');
+	const [description, setDescription] = useState('');
+	const [price, setPrice] = useState();
+	const [file, setFile] = useState(null);
 
-	const addType = () => {
-		createType({ name: value }).then(data => {
-			setValue('');
-			onHide();
-		});
-	}; */
+	useEffect(() => {
+		fetchTypes().then(data => service.setTypes(data));
+	}, [service]);
+
+	const selectFile = e => {
+		setFile(e.target.files[0]);
+	};
+
+	const addService = () => {
+		const formData = new FormData();
+		formData.append('name', name);
+		formData.append('description', description);
+		formData.append('price', `${price}`);
+		formData.append('img', file);
+		formData.append('typeId', service.selectedType.id);
+		createService(formData).then(data => onHide());
+	};
 
 	return (
 		<Modal show={show} onHide={onHide} centered>
-			<Modal.Header closeButton>
-				<Modal.Title id='contained-modal-title-vcenter'>Добавить тип</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
+			<div className='modal-header'>
+				<div className='modal-title'>
+					Свяжемся с Вами
+					<br /> в ближайшее время!
+				</div>
+				<img className='modal-x' src={close} alt='Закрыть' onClick={onHide} />
+			</div>
+			<div className='modal-body'>
+				<div className='modal-subtitle'>Заполните форму для получения консультации</div>
 				<Form>
-					<Form.Control value={value} onChange={e => setValue(e.target.value)} placeholder={'Введите название типа'} />
+					<Form.Control
+						value={name}
+						onChange={e => setName(e.target.value)}
+						className='form form-name'
+						placeholder='Имя'
+					/>
+
+					<Form.Control
+						value={name}
+						onChange={e => setName(e.target.value)}
+						className='form form-name'
+						placeholder='Номер телефона'
+					/>
+
+					<Form.Control
+						as='textarea'
+						value={description}
+						onChange={e => setDescription(e.target.value)}
+						className='form form-description'
+						placeholder='Комментарий'
+					/>
+
+					<div class='checkbox'>
+						<input type='checkbox' id='checkbox' className='modal-checkbox' />
+						<label  className='modal-checkbox-label'>
+							Даю согласие на обработку персональных данных
+						</label>
+					</div>
+
+					<button className='modal-button'>Перезвоните мне</button>
 				</Form>
-			</Modal.Body>
-			<Modal.Footer>
-				<Button variant='outline-danger' onClick={onHide}>
-					Закрыть
-				</Button>
-				<Button variant='outline-success' onClick={addType}>
-					Добавить
-				</Button>
-			</Modal.Footer>
+			</div>
 		</Modal>
 	);
-};
+});
 
 export default SubmitApplication;
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,19 +1,24 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useState } from 'react';
+import SubmitApplication from '../components/modals/SubmitApplication';
 import '../styles/main.css';
 
 
 const Main = observer(() => {
+	const [applicationVisible, setApplicationVisible] = useState(false);
+
 	return (
-			<main className='home-main'>
-				<div className='wrapper'>
-					<section className='offer'>
-						<h1>Строительные и пусконаладочные работы</h1>
-						<p>Комплексный подход к решению задач любого уровня в один клик. Действуй!</p>
-						<button>Получить консультацию</button>
-					</section>
-				</div>
-			</main>
+		<main className='home-main'>
+			<div className='wrapper'>
+				<section className='offer'>
+					<h1>Строительные и пусконаладочные работы</h1>
+					<p>Комплексный подход к решению задач любого уровня в один клик. Действуй!</p>
+					<button onClick={() => setApplicationVisible(true)}>Получить консультацию</button>
+				</section>
+			</div>
+
+			<SubmitApplication show={applicationVisible} onHide={() => setApplicationVisible(false)}></SubmitApplication>
+		</main>
 	);
 });
 
