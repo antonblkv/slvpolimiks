9f786ce3355ea4b9ccab647acf9a7312625e80d7
Anton <antonblkv@yandex.ru>
2024-05-02 21:36:08 +0300

    add userName and userPhone in table admin

--- client/src/components/OrderItem.js
+++ client/src/components/OrderItem.js
@@ -1,17 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { fetchOneService } from '../http/serviceAPI';
+import { fetchOneUser } from '../http/userAPI';
 import { Context } from '../index';
 
 const OrderItem = ({ order }) => {
+	const { user } = useContext(Context);
+
+
+	const [currentUser, setCurrentUser] = useState({});
 	const [service, setService] = useState({});
 
 	useEffect(() => {
 		fetchOneService(order.serviceId).then(data => setService(data));
 	}, []);
 
+	useEffect(() => {
+		fetchOneUser(order.userId).then(data => setCurrentUser(data));
+	}, []);
+
 	return (
 		<tr className='row-orders'>
 			<td className='number-order'>{order.id}</td>
+			{user.isAdmin ? (
+				<>
+					<td className='name-user'>{currentUser.name}</td>
+					<td className='phone-user'>{currentUser.phone}</td>
+				</>
+			) : (
+				''
+			)}
 			<td className='name-order'>{service ? service.name : 'Первичный звонок'}</td>
 			<td className='status-order'>{order.status}</td>
 			<td className='comment-order'>
--- client/src/components/TableOrders.js
+++ client/src/components/TableOrders.js
@@ -1,27 +1,35 @@
-import React, { useContext } from 'react';
 import { observer } from 'mobx-react-lite';
+import React, { useContext } from 'react';
 import { Context } from '../index';
 import OrderItem from './OrderItem';
 
 const TableOrders = observer(() => {
 	const { order } = useContext(Context);
-	
+	const { user } = useContext(Context);
+
 	return (
 		<table className='table-orders'>
-							<thead className='head-orders'>
-								<tr className='row-orders'>
-									<th className='th-number'>№</th>
-									<th className='th-name'>Услуга</th>
-									<th className='th-status'>Статус</th>
-									<th className='th-comment'>Комментарий</th>
-								</tr>
-							</thead>
-							
-							{order.orders.map(order => (
+			<thead className='head-orders'>
+				<tr className='row-orders'>
+					<th className='th-number'>№</th>
+					{user.isAdmin ? (
+						<>
+							<th className='th-status'>Имя</th>
+							<th className='th-status'>Телефон</th>
+						</>
+					) : (
+						''
+					)}
+					<th className='th-name'>Услуга</th>
+					<th className='th-status'>Статус</th>
+
+					<th className='th-comment'>Комментарий</th>
+				</tr>
+			</thead>
+			{order.orders.map(order => (
 				<OrderItem key={order.id} order={order} />
 			))}
-							</table>
-	
+		</table>
 	);
 });
 
